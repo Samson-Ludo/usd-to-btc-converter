d9af232578759cdc7f90b37499fc7ba1704aac7d
Samson Okaludo <samson@divercity.io>
2024-09-06 22:38:07 +0100

    feat: render BTCConverter

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import BTCConverter from "../components/BTCConverter";
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <BTCConverter />
+    </div>
+  );
+};
+
+export default Home;
